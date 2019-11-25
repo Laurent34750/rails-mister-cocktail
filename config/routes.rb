7d574499fb924ab6d765277383a2309e755884b9
Laurent Deguil <ldeguil@yahoo.com>
2019-11-25 17:40:43 +0100

    cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
+  get 'cocktails', to: 'cocktails#index'
+  get 'cocktails/:id', to: 'cocktails#show'
+  get 'cocktails/new', to: 'cocktails#new'
+  post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails/:id/edit', to: 'cocktails#edit'
 end
